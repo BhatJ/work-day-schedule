187c18f1391ee584f637eb359e5aac6a58716b3e
BhatJ <jisha2209@gmail.com>
2022-12-12 16:47:09 +1100

    Added a lister for click events on save buttons
    Saved events to local storage using the hour element id as the key

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,9 @@ var businessHours = [
 
 
 /*
- * A function to render all business hours.
- * The style of each hour is initially unset.
+ * A function to render all business hour elements.
+ * The style of each element is initially unset.
+ * One element for each hour from 9am to 5pm.
  */
 function renderBusinessHours() {
   var schedulerBdy = $('#scheduler');
@@ -61,14 +62,23 @@ function renderBusinessHours() {
     hourDiv.append($("<textarea class=\"col-8 col-md-10 description\" rows=\"3\"> </textarea>"));
     hourDiv.append($("<button class=\"btn saveBtn col-2 col-md-1\" aria-label=\"save\"><i class=\"fas fa-save\" aria-hidden=\"true\"></i></button>"));
     schedulerBdy.append(hourDiv);
+    localStorage.setItem(businessHours[i].id, '');
   }
+
+  console.log("renderBusinessHours() executed");
 }
 
+/*
+ * A function to style the business hour elements.
+ * The style of each element is set based on the current hour of the day.
+ * Past elements (hour < current hour of the day) - use 'past' style
+ * Current element (hour == current hour of the day) - use 'present' style
+ * Future elements (hour > current hour of the day) - use 'future' style
+ */
 function styleBusinessHours(hour) {
 
   for (var i = 0; i < businessHours.length; i++) {
     var hourEl = $("#" + businessHours[i].id);
-    console.log(hourEl);
     if (businessHours[i].hour < hour)
     {
       hourEl.addClass('past');
@@ -79,28 +89,31 @@ function styleBusinessHours(hour) {
       hourEl.addClass('future');
     }
   }
+
+  console.log("styleBusinessHours() executed");
 }
 
 $(function () {
 
+  // Use Day.js to get the current hour of the day on 24-hr format
   var currentHour = dayjs().format('H');
 
+  // Render the business hour elements. 
   renderBusinessHours();
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
+  // Style the hour elements
   styleBusinessHours(currentHour);
+  
+  // An event lister for all save buttons
+  // Creates a lister for all elements using the 'saveBtn' class
+  $(".saveBtn").click(function() {
+    var workEvent = $(this).siblings()[1].value;
+    var hourId = $(this).parent().attr('id');
+
+    localStorage.setItem(hourId, workEvent);
+  });
+  
+  
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
